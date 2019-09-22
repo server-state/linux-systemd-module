3461f133c5198a2ba5d2b7b35555e27fda523d67
Ludwig Richter <richter@fliegwerk.com>
2019-09-22 19:58:29 +0200

    Implemented systemd unit test before and after

--- tests/install-systemd-units.sh
+++ tests/install-systemd-units.sh
@@ -1,2 +1,15 @@
 #!/bin/bash
+# Simple install script for systemd testing units
+# ================================================
+# Copyright (C) server-state
 
+SYSTEMD_UNITS="/usr/local/lib/systemd/system"
+#SYSTEMD_UNITS="./systemd/units"
+
+GIVEN_UNITS="./tests/units"
+
+# install given units to test system
+mkdir -p "$SYSTEMD_UNITS"
+cp -r "$GIVEN_UNITS/." "$SYSTEMD_UNITS"
+
+systemctl daemon-reload
